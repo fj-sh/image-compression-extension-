887d9644166bdf6b8e7bb53af18d46f87b8458cc
fj <menspocky322@gmail.com>
2022-08-04 12:05:17 +0900

    リリースした

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# 機能追加の予定
+
+- [ ] 変換クオリティを設定するSliderを作成
+  - [Vite+React+TypeScript で、UIフレームワークにMUI(v5)を導入する。](https://zenn.dev/longbridge/articles/bba17785710c1a)
+  - MUI の [Slider](https://mui.com/material-ui/react-slider/) を使う
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { crx, defineManifest } from "@crxjs/vite-plugin";
-
+import { crx, defineManifest } from '@crxjs/vite-plugin'
 
 const manifest = defineManifest({
   manifest_version: 3,
-  name: "Open Bookmarks",
-  version: "1.0.0",
-  permissions: ["tabs"],
-  action: {
-    default_popup: "index.html",
+  name: 'Webp Converter',
+  version: '1.0.0',
+  icons: {
+    '16': 'compress.png',
+    '48': 'compress.png',
+    '128': 'compress.png',
   },
-});
+  action: {
+    default_popup: 'index.html',
+  },
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), crx({ manifest })]
+  plugins: [react(), crx({ manifest })],
 })
